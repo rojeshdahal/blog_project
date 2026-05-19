70d46c24c7855c470a23f643ff00e388ff31db68
Rojesh Babu Dahal <dahalrojesh12@gmail.com>
2026-05-19 18:46:48 +0545

    post detail page created

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_detail
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('posts/<int:id>/', post_detail, name= 'post_detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -7,4 +7,11 @@ def post_list(request):
 
     return render(request, "posts/post_list.html", {
         "posts": posts
+    })
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    return render(request, "posts/post_detail.html", {
+        "post": post
     })
