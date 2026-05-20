cab2457844a83089c6387d5b89169ffc09680a98
Rojesh Babu Dahal <dahalrojesh12@gmail.com>
2026-05-20 21:04:13 +0545

    user can register, login and create posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_list, post_detail, create_post
 
 urlpatterns = [
     path('', post_list, name='post_list'),
-    path('posts/<int:id>/', post_detail, name= 'post_detail'),
+    path('posts/<int:id>/', post_detail, name='post_detail'),
+    path('create/', create_post, name='create_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm, Post
 
 def post_list(request):
     posts = Post.objects.all()
@@ -14,4 +14,27 @@ def post_detail(request, id):
 
     return render(request, "posts/post_detail.html", {
         "post": post
+    })
+
+@login_required
+def create_post(request):
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+
+            post = form.save(commit=False)
+
+            post.author = request.user
+
+            post.save()
+
+            return redirect('post_list')
+
+    else:
+        form = PostForm()
+
+    return render(request, 'posts/create_post.html', {
+        'form': form
     })
