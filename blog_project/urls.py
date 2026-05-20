dc541ff514aa2de2e11e120748bc56a8b28ffe67
Rojesh Babu Dahal <dahalrojesh12@gmail.com>
2026-05-20 20:16:29 +0545

    registration and login pages

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
-]
 
+    path('', include('posts.urls')),
+
+    path('accounts/', include('accounts.urls')),
+
+    path('accounts/', include('django.contrib.auth.urls')),
+]
