7236737fb573bed84d8da93f37891e8b18490320
Rojesh Babu Dahal <dahalrojesh12@gmail.com>
2026-05-23 18:56:45 +0545

    user can edit and delete posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import post_list, post_detail, create_post
-
+from .views import (
+    post_list,
+    post_detail,
+    create_post,
+    edit_post,
+    delete_post
+)
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('posts/<int:id>/', post_detail, name='post_detail'),
     path('create/', create_post, name='create_post'),
+    path('posts/<int:id>/edit/', edit_post, name='edit_post'),
+    path('posts/<int:id>/delete/', delete_post, name='delete_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm, Post
+from django.http import HttpResponseForbidden
+
+from .models import Post
+
 
 def post_list(request):
+
     posts = Post.objects.all()
 
     return render(request, "posts/post_list.html", {
@@ -37,4 +42,45 @@ def create_post(request):
 
     return render(request, 'posts/create_post.html', {
         'form': form
+    })
+
+@login_required
+def edit_post(request, id):
+
+    post = get_object_or_404(Post, id=id)
+
+    if post.author != request.user:
+        return HttpResponseForbidden("You are not allowed to edit this post.")
+
+    if request.method == "POST":
+
+        form = PostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('post_detail', id=post.id)
+
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'posts/edit_post.html', {
+        'form': form
+    })
+
+@login_required
+def delete_post(request, id):
+
+    post = get_object_or_404(Post, id=id)
+
+    if post.author != request.user:
+        return HttpResponseForbidden("You are not allowed to delete this post.")
+
+    if request.method == "POST":
+        post.delete()
+
+        return redirect('post_list')
+
+    return render(request, 'posts/delete_post.html', {
+        'post': post
     })
