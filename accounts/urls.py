8e2d1ed133065de730b1c0bb5b49dfa86424a68d
Rojesh Babu Dahal <dahalrojesh12@gmail.com>
2026-05-25 21:07:20 +0545

    user profile updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import register
+from .views import register, edit_profile
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('profile/edit/', edit_profile, name='edit_profile'),
 ]
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
 
 
 def register(request):
@@ -17,3 +19,34 @@ def register(request):
     return render(request, 'accounts/register.html', {
         'form': form
     })
+
+@login_required
+def edit_profile(request):
+
+    profile = request.user.profile
+
+    if request.method == 'POST':
+
+        form = ProfileForm(
+            request.POST,
+            request.FILES,
+            instance=profile
+        )
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('post_list')
+
+    else:
+
+        form = ProfileForm(instance=profile)
+
+    return render(
+        request,
+        'accounts/edit_profile.html',
+        {
+            'form': form
+        }
+    )
